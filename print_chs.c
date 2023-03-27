cac00575341e22576baa4c4627eb7c884203ee6e
Elahizes <eliashizes@gmail.com>
2023-03-27 17:00:24 +0300

    printf

---
+++ print_chs.c
@@ -0,0 +1,118 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_c - function that prints a character
+ * @c: char of variable
+ * Return: number of characters printed
+ */
+
+int print_c(va_list c)
+{
+	_putchar(va_arg(c, int));
+
+	return (1);
+}
+
+/**
+ * print_s - function that prints a string
+ * @s: operator and pointer
+ * Return: number of characters printed
+ */
+
+int print_s(va_list s)
+
+{
+	unsigned int i;
+	char *str;
+
+	str = va_arg(s, char *);
+	i = 0;
+	if (str == NULL)
+		str = "(null)";
+
+	while (str[i] != '\0')
+	{
+		_putchar (str[i]);
+		i++;
+	}
+		return (i);
+}
+/**
+ * hex_print - prints a char's ascii value in uppercase hex
+ * @c: char to print
+ *
+ * Return: number of chars printed (always 2)
+ */
+static int hex_print(char c)
+{
+	int count;
+	char diff = 'A' - ':';
+	char d[2];
+
+	d[0] = c / 16;
+	d[1] = c % 16;
+	for (count = 0; count < 2; count++)
+	{
+		if (d[count] >= 10)
+			_putchar('0' + diff + d[count]);
+		else
+			_putchar('0' + d[count]);
+	}
+	return (count);
+}
+/**
+ * print_S - prints a string and nonprintable character ascii values
+ * @S: string to print
+ *
+ * Return: number of chars printed
+ */
+int print_S(va_list S)
+{
+	unsigned int i;
+	int count = 0;
+	char *str = va_arg(S, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] < 32 || str[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			count += 2;
+			count += hex_print(str[i]);
+		}
+		else
+		{
+			_putchar(str[i]);
+			count++;
+		}
+	}
+	return (count);
+}
+
+/**
+ * print_r - prints astring in reverse
+ * @r: string to print
+ *
+ * Return: number of chars printed
+ */
+int print_r(va_list r)
+{
+	char *str;
+	int i, count = 0;
+
+	str = va_arg(r, char *);
+	if (str == NULL)
+		str = ")llun(";
+	for (i = 0; str[i]; i++)
+		;
+	for (i -= 1; i >= 0; i--)
+	{
+		_putchar(str[i]);
+		count++;
+	}
+	return (count);
+}
